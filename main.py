1705185237b46c9a1b9213f12f9735e9460e002a
ChYf <chizzy2205@gmail.com>
2025-03-22 14:43:49 +0100

    Edited to prompt user to try again in 24 hours

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ async def faucet(interaction: discord.Interaction, address: str):
 
     # ✅ Send success or failure message
     if tx_transfer:
-        await interaction.followup.send(f"✅ Faucet success!\n Try again in 24 hours \n Transaction Hash: {EXPLORER}tx/{tx_transfer}")
+        await interaction.followup.send(f"✅ Faucet success! Try again in 24 hours! \n Transaction Hash: {EXPLORER}tx/{tx_transfer}")
     else:
         await interaction.followup.send("❌ Transaction failed. Try again later.", ephemeral=True)
 
